4ae1579c91753166dedc226b3840b726e93dd3ff
Atif <75267830+atif-b@users.noreply.github.com>
2021-11-18 01:26:40 +0000

    Started home page
    Login and signup buttons

--- bumpInto/App.js
+++ bumpInto/App.js
@@ -1,19 +1,48 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
 
-const textStyle = StyleSheet.create({
+const styles = StyleSheet.create({
   center: {
     alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+  },
+
+  red: {
+    color: 'red',
+  },
+
+  buttonCtn: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+  button: {
+    backgroundColor: '#4abc93',
+    padding: 10,
+    width: '45%',
+  },
+
+  buttonTxt: {
+    color: 'white',
   },
 });
 
 const BumpInto = () => {
-  const x = 'bump app';
-  console.log(x);
+  //const x = 'bump app';     /***** use this to debug in console *****/
+  //console.log(x);
 
   return (
-    <View style={[textStyle.center, {top: 100}]}>
-      <Text>"Bump Into"</Text>
+    <View style={styles.center}>
+      <Text style={styles.red}>"Bump Into"</Text>
+      <View style={styles.buttonCtn}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonTxt}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonTxt}>Signup</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
