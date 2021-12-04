7a9853d627fd759ed03a406aee865a108ed38c0e
Atif <75267830+atif-b@users.noreply.github.com>
2021-12-04 02:06:18 +0000

    Login design

--- bumpInto/screens/Login.js
+++ bumpInto/screens/Login.js
@@ -1,21 +1,104 @@
-import React from 'react';
-import {Text, View, StyleSheet, TouchableOpacity, Button} from 'react-native';
+import React, {useContent, useState} from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Button,
+  Image,
+  TextInput,
+  ScrollView,
+} from 'react-native';
+import FormInput from '../components/FormInput';
+import FormButton from '../components/FormButton';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {useNavigation} from '@react-navigation/native';
 
 const Login = ({navigation}) => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Login</Text>
-      <Button title="Log in" onPress={() => navigation.navigate('Home')} />
-      <Button
-        title="Register"
-        onPress={() => navigation.navigate('Register')}
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.text}>Bump Into</Text>
+
+      <FormInput
+        labelValue={email}
+        onChangeText={userEmail => setEmail(userEmail)}
+        placeholderText="Email"
+        keyboardType="email-address"
+        autoCapitalize="none"
+        autoCorrect={false}
       />
-      {/* Title is what the button says. naviagation.naviagte must have stack.screen name (that is from stack.nav) */}
-    </View>
+
+      <FormInput
+        labelValue={password}
+        onChangeText={userPassword => setPassword(userPassword)}
+        placeholderText="Password"
+        iconType="lock"
+        secureTextEntry={true}
+      />
+
+      <FormButton
+        buttonTitle="Sign In"
+        // onPress={() => LoginCheck(email, password)}
+        onPress={() => navigation.navigate('Home')}
+      />
+
+      <TouchableOpacity style={styles.forgotButton} onPress={() => {}}>
+        <Text style={styles.navButtonText}>Forgot Password?</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.forgotButton}
+        onPress={() => navigation.navigate('Signup')}>
+        <Text style={styles.navButtonText}>
+          Don't have an acount? Create here
+        </Text>
+      </TouchableOpacity>
+    </ScrollView>
   );
 };
 
+//     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+//       <Text>Login</Text>
+//       <TextInput />
+//       <Button title="Log in" onPress={() => navigation.navigate('Home')} />
+//       <Button
+//         title="Register"
+//         onPress={() => navigation.navigate('Register')}
+//       />
+//       {/* Title is what the button says. navigation.navigate must have stack.screen name (that is from stack.nav) */}
+//     </View>
+//   );
+// };
+
 export default Login;
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    paddingTop: 50,
+  },
+  text: {
+    //fontFamily: 'Kufam-SemiBoldItalic',
+    fontSize: 28,
+    marginBottom: 10,
+    color: '#051d5f',
+  },
+  navButton: {
+    marginTop: 15,
+  },
+  forgotButton: {
+    marginVertical: 35,
+  },
+  navButtonText: {
+    fontSize: 18,
+    fontWeight: '500',
+    color: '#2e64e5',
+    //fontFamily: 'Lato-Regular',
+  },
+});
